51a1b4fa238818e0d174e80bbd65b991d7468495
Nick Shulhin <nickita.shulgin@yahoo.com>
2018-08-25 22:40:41 +1000

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-app.use(express.static('/Users/admin/Desktop/sw/'));
+app.use(express.static('/'));
 
-app.get('/', (req, res) =>  res.sendFile(path.join('/Users/admin/Desktop/sw/index.html')));
+app.get('/', (req, res) =>  res.sendFile(path.join('/index.html')));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
